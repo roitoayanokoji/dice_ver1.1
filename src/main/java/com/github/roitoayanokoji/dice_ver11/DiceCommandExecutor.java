85fc38e3b1b4f112a3725656603abf29956ed46a
senganull <ayanokoji.roito@gmail.com>
2024-12-24 18:12:25 +0900

    broadcastmessage

--- src/main/java/com/github/roitoayanokoji/dice_ver11/DiceCommandExecutor.java
+++ src/main/java/com/github/roitoayanokoji/dice_ver11/DiceCommandExecutor.java
@@ -1,4 +1,54 @@
 package com.github.roitoayanokoji.dice_ver11;
 
-public class DiceCommandExecutor {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Random;
+
+public class DiceCommandExecutor implements CommandExecutor {
+
+    private final Dice_ver11 plugin;
+
+    public DiceCommandExecutor(Dice_ver11 plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+        if (command.getName().equalsIgnoreCase("dice")){
+            if (!(sender instanceof Player)){
+                sender.sendMessage("プレイヤーのみ実行可能");
+            }
+            Player player = (Player) sender;
+
+            int sides = 6;
+            //ダイス面の初期値
+            if (args.length > 0){
+                try {
+                    sides = Integer.parseInt(args[0]);
+                    if (sides <= 0){
+                        player.sendMessage("０面ダイスってなんやねん");
+                        return true;
+                    }
+                } catch (NumberFormatException e) {
+                    player.sendMessage("指定が間違っています。");
+                    return true;
+                }
+            }
+            //ダイスの処理
+            int random = new Random().nextInt(sides) + 1;
+            String dicemessage = player.getName() + "が" + sides + "面ダイスを振って" + random + "が出た";
+            String pl = "[Dice]";
+
+            //プレイヤーへのメッセージ
+            player.sendMessage(pl + "結果 : " + random);
+
+            //broadcast
+            plugin.getServer().broadcast(pl +dicemessage,"");
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/java/com/github/roitoayanokoji/dice_ver11/Dice_ver11.java
+++ src/main/java/com/github/roitoayanokoji/dice_ver11/Dice_ver11.java
@@ -7,11 +7,16 @@ public final class Dice_ver11 extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("OnEnable");
+
+        //
+        getCommand("dice").setExecutor(new DiceCommandExecutor(this));
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("OnDisable");
     }
 }
